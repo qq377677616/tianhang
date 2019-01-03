4c1897b21d8bc44b87a40b5b8cfe7d589c323ccc
songziquan <377677616@qq.com>
2019-01-03 17:42:09 +0800

    5个

--- src/components/lottery/home/home.vue
+++ src/components/lottery/home/home.vue
@@ -144,6 +144,7 @@ import { getCreateIssue, getCurrentStage, purchase, getAwardRecord } from "api/r
       console.log(377677616)
       console.log(377677616)
       console.log(377677616)
+      console.log(377677616)
     },
     activated() {
       /*if (!this.isInitData) {
